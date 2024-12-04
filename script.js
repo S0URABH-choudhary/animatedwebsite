1e85a5fed8b56de8ce69bdd7ec3a968b85677f81
S0URABH_choudhary <subh7898765@gmail.com>
2024-12-04 11:36:15 +0530

    complete

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-
-
+const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
+});
 
 // animating nav section
 gsap.from(".nlink",{
@@ -18,58 +20,6 @@ gsap.from(".anim",{
     opacity:0,
     ease:Expo,
     duration:2,
-})
-// animating motive section 
-
-gsap.from(".motive h3",{
-    x:-30,
-    opacity:0,
-    duration:2,
-    ease:Expo,
-    scrollTrigger:{
-        trigger:".motive h3",
-        scroller:"body",
-        start:"top 80%"
-    }
-})
-gsap.from(".motive p",{
-    x:30,
-    opacity:0,
-    duration:2,
-    ease:Expo,
-    scrollTrigger:{
-        trigger:".motive h3",
-        scroller:"body",
-        start:"top 80%"
-    }
-})
-
-// animating the health section 
-
-gsap.from(".health>h3",{
-    x:-30,
-    opacity:0,
-    duration:2,
-    ease:Power1,
-    scrollTrigger:{
-        trigger:".health>h3",
-        scroller:"body",
-        start:"top 80%"
-    }
-})
-
-gsap.from(".sanim",{
-    y:40,
-    opacity:0,
-    ease:Expo, 
-    duration:2,   
-    scrollTrigger:{
-        trigger:".health>h3",
-        scroller:"body",
-        start:"top 80%"
-    }
-
-
 })
 
 // animating footer video 
